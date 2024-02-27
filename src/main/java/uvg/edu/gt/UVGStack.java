7f91d5384a28ce8bc96e09ec74385d8841feb9de
TonitoMC <138615863+TonitoMC@users.noreply.github.com>
2024-02-26 21:15:02 -0600

    Add files via upload
    Commit inicial, implementada clase Calculator, StackFactory, StackArrayList y StackVector

---
+++ src/main/java/uvg/edu/gt/UVGStack.java
@@ -0,0 +1,14 @@
+package uvg.edu.gt;
+
+import java.util.ArrayList;
+
+public interface UVGStack <T>{
+    public void push(T item);
+
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+
+    public ArrayList<T> getList();
+
+}
